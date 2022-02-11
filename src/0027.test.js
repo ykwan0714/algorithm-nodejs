631ab698cebbaad2d17e23b73c05196829ebabfc
x8128 <aliasbonfire@gmail.com>
2022-02-11 23:55:16 +0900

    doublyLinkedList unshift

---
+++ src/0027.test.js
@@ -0,0 +1,59 @@
+const DoublyLinkedList = require("./0027");
+
+const doublyLinkedList = new DoublyLinkedList();
+
+test(`doublyLinkedList.unshift(5)`, () => {
+  expect(doublyLinkedList.unshift(5)).toEqual(doublyLinkedList);
+});
+
+test(`doublyLinkedList.length`, () => {
+  expect(doublyLinkedList.length).toEqual(1);
+});
+
+test(`doublyLinkedList.head.val`, () => {
+  expect(doublyLinkedList.head.val).toEqual(5);
+});
+
+test(`doublyLinkedList.tail.val`, () => {
+  expect(doublyLinkedList.tail.val).toEqual(5);
+});
+
+test(`doublyLinkedList.unshift(10)`, () => {
+  expect(doublyLinkedList.unshift(10)).toEqual(doublyLinkedList);
+});
+
+test(`doublyLinkedList.length`, () => {
+  expect(doublyLinkedList.length).toEqual(2);
+});
+
+test(`doublyLinkedList.head.val`, () => {
+  expect(doublyLinkedList.head.val).toEqual(10);
+});
+
+test(`doublyLinkedList.head.next.val`, () => {
+  expect(doublyLinkedList.head.next.val).toEqual(5);
+});
+
+test(`doublyLinkedList.tail.val`, () => {
+  expect(doublyLinkedList.tail.val).toEqual(5);
+});
+
+test(`doublyLinkedList.unshift(15)`, () => {
+  expect(doublyLinkedList.unshift(15)).toEqual(doublyLinkedList);
+});
+
+test(`doublyLinkedList.length`, () => {
+  expect(doublyLinkedList.length).toEqual(3);
+});
+
+test(`doublyLinkedList.head.val`, () => {
+  expect(doublyLinkedList.head.val).toEqual(15);
+});
+
+test(`doublyLinkedList.tail.val`, () => {
+  expect(doublyLinkedList.tail.val).toEqual(5);
+});
+
+test(`doublyLinkedList.head.next.next.val`, () => {
+  expect(doublyLinkedList.head.next.next.val).toEqual(5);
+});
