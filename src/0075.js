52937a52f02193ae52632ee7dc03d691788c1914
x8128 <aliasbonfire@gmail.com>
2022-02-13 22:05:42 +0900

    graph dijkstra

---
+++ src/0075.js
@@ -0,0 +1,70 @@
+function WeightedGraph() {}
+
+/***
+ *** Use Graph as a constructor for WeightedGraph to inherit from!
+ ***
+ ***/
+
+function Graph() {
+  this.adjacencyList = {};
+}
+
+Graph.prototype.numEdges = function () {
+  let total = 0;
+
+  Object.values(this.adjacencyList).forEach((list) => {
+    total += list.length;
+  });
+
+  // note that we've double-counted up til now since we've looked at
+  // the adjacencyList for every node.
+  return total / 2;
+};
+
+Graph.prototype.addVertex = function (vertex) {
+  this.adjacencyList[vertex] = [];
+};
+
+Graph.prototype.addEdge = function (vertex1, vertex2) {
+  this.adjacencyList[vertex1].push(vertex2);
+  this.adjacencyList[vertex2].push(vertex1);
+};
+
+Graph.prototype.removeVertex = function (vertex) {
+  while (this.adjacencyList[vertex].length) {
+    const adjacentVertex = this.adjacencyList[vertex].pop();
+    this.removeEdge(adjacentVertex, vertex);
+  }
+  delete this.adjacencyList[vertex];
+};
+
+Graph.prototype.removeEdge = function (vertex1, vertex2) {
+  this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+    (v) => v !== vertex2
+  );
+  this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+    (v) => v !== vertex1
+  );
+};
+
+/***
+ *** Use the following as a PriorityQueue (it's a min heap)!
+ ***
+ ***/
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+module.exports = WeightedGraph;
---
+++ src/0075.test.js
@@ -0,0 +1,45 @@
+const WeightedGraph = require("./0075");
+
+const g = new WeightedGraph();
+
+g.addVertext("A");
+g.addVertext("Z");
+g.addVertext("C");
+g.addVertext("D");
+g.addVertext("E");
+g.addVertext("H");
+g.addVertext("Q");
+g.addVertext("G");
+
+g.addEdge("A", "Z", 7);
+g.addEdge("A", "C", 8);
+
+g.addEdge("Z", "Q", 2);
+
+g.addEdge("C", "G", 4);
+
+g.addEdge("D", "Q", 8);
+
+g.addEdge("E", "H", 1);
+
+g.addEdge("H", "Q", 3);
+
+g.addEdge("Q", "C", 6);
+
+g.addEdge("G", "Q", 9);
+
+test(`g.Dijkstra("A", "E")`, () => {
+  expect(g.Dijkstra("A", "E")).toEqual(["A", "Z", "Q", "H", "E"]);
+});
+
+test(`g.Dijkstra("A", "Q")`, () => {
+  expect(g.Dijkstra("A", "Q")).toEqual(["A", "Z", "Q"]);
+});
+
+test(`g.Dijkstra("A", "G")`, () => {
+  expect(g.Dijkstra("A", "G")).toEqual(["A", "C", "G"]);
+});
+
+test(`g.Dijkstra("A", "D")`, () => {
+  expect(g.Dijkstra("A", "D")).toEqual(["A", "Z", "Q", "D"]);
+});
